536418b12db939c7b709bc71db291575dbfe9797
cRaeber <54545269+cRaeber@users.noreply.github.com>
2021-10-07 11:06:20 -0500

    Updated DegreePath tab

--- client/src/components/Questions/Tabs/DegreePath.js
+++ client/src/components/Questions/Tabs/DegreePath.js
@@ -6,6 +6,7 @@ import FormControlLabel from "@mui/material/FormControlLabel";
 import FormControl from "@mui/material/FormControl";
 import FormLabel from "@mui/material/FormLabel";
 
+
 export default function DegreePath() {
   return (
     <Container
@@ -16,26 +17,61 @@ export default function DegreePath() {
       }}
     >
       <Typography variant="h2">Degree paths</Typography>
-      <Typography>certificates and paths go here.</Typography>
+      <Typography>Please select your desired degree path.</Typography>
       <FormControl component="fieldset">
-        <FormLabel component="legend">Degree Path</FormLabel>
-        <RadioGroup defaultValue="traditional" name="radio-buttons-group">
+        <FormLabel component="legend">Degree Path:</FormLabel>
+        <RadioGroup defaultValue="traditional" name="type-path-group">
           <FormControlLabel
             value="traditional"
             control={<Radio />}
             label="Traditional"
           />
           <FormControlLabel
-            value="certificate"
-            control={<Radio />}
-            label="Certificate"
-          />
-          <FormControlLabel
             value="professional"
             control={<Radio />}
             label="Professional"
           />
-        </RadioGroup>
+          <FormControlLabel
+            value="certificate"
+            control={<Radio />}
+            label="Certificate"
+          />
+          </RadioGroup>
+
+          <FormLabel component="legend">Certificate Options:</FormLabel>
+
+            <RadioGroup defaultValue="none" name="type-certificate-group" >
+            <FormControlLabel
+              value="artificial-intelligence"
+              control={<Radio />}
+              label="Artificial Intelligence"
+            />
+            <FormControlLabel
+              value="cybersecurity"
+              control={<Radio />}
+              label="Cybersecurity"
+            />
+            <FormControlLabel
+            value="data-science"
+            control={<Radio />}
+            label="Data Science"
+            />
+            <FormControlLabel
+              value="internet-and-web"
+              control={<Radio />}
+              label="Internet and Web"
+            />
+            <FormControlLabel
+              value="mobile-apps-and-computing"
+              control={<Radio />}
+              label="Mobile Apps and Computing"
+            />
+            <FormControlLabel              
+              value="none"
+              control={<Radio />}
+              label="None"
+            />
+          </RadioGroup>          
       </FormControl>
     </Container>
   );

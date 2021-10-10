f188c01c3a9f8b0f0ee2d8126e1aedd115325090
Gabriel Wallace <gabe.wallace04@gmail.com>
2021-10-09 23:22:18 -0500

    Add checkbox labels from array

--- client/src/components/Questions/Tabs/CompletedCourses.js
+++ client/src/components/Questions/Tabs/CompletedCourses.js
@@ -1,7 +1,29 @@
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
+import FormGroup from "@mui/material/FormGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import courses from "../../../data/ClassInfo.json";
+import parseCSVFile from "../../../csvParser";
+import rotationCSV from "../../../data/rotation_cleaned.csv";
+import { FormControl, FormLabel } from "@mui/material";
+
+var data = parseCSVFile(rotationCSV);
 
 export default function CompletedCourses() {
+  var rotation = data[0];
+  let classCodes = rotation.map(a => a.Discipline + " " + a.Number);
+
+  const options = classCodes.map((item) => {
+    return (
+      <FormControlLabel key={item} value={item} control={<Checkbox/>} label={item}>
+        {item}
+      </FormControlLabel>
+      )
+    });
+
+  console.log(options);
+
   return (
     <Container
       sx={{
@@ -12,6 +34,13 @@ export default function CompletedCourses() {
     >
       <Typography variant="h2">Completed and Transferred Courses</Typography>
       <Typography>Course stuff here</Typography>
+      <FormGroup>
+        <FormLabel>Select Courses</FormLabel>
+        <FormControlLabel
+          control={<Checkbox />}
+          label={options}
+        />
+      </FormGroup>
     </Container>
   );
 }

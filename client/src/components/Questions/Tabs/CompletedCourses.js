d2cd330720a9bdff628b39948eccdb658717e08e
Gabriel Wallace <35503105+gwallace04@users.noreply.github.com>
2021-10-11 20:36:28 -0500

    Feature/completed tab (#38)
    * Remove trailing empty line in rotation_cleaned.csv
    
    * Add checkbox labels from array
    
    * Remove extraneous checkbox
    
    * Read class info from ClassInfo.json instead of the rotation csv
    
    * Fix minor code formatting
    
    * Make description more verbose and remove unnecassary console.log line

--- client/src/components/Questions/Tabs/CompletedCourses.js
+++ client/src/components/Questions/Tabs/CompletedCourses.js
@@ -1,7 +1,23 @@
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
+import FormGroup from "@mui/material/FormGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import courses from "../../../data/ClassInfo.json";
 
 export default function CompletedCourses() {
+  let courseLabels = courses.map(
+    a => a.subject + " " + a.courseNum + " - " + a.courseName
+    );
+
+  const options = courseLabels.map((item) => {
+    return (
+      <FormControlLabel key={item} value={item} control={<Checkbox/>} label={item}>
+        {item}
+      </FormControlLabel>
+      )
+    });
+
   return (
     <Container
       sx={{
@@ -11,7 +27,10 @@ export default function CompletedCourses() {
       }}
     >
       <Typography variant="h2">Completed and Transferred Courses</Typography>
-      <Typography>Course stuff here</Typography>
+      <Typography>Select courses already completed/in progress</Typography>
+      <FormGroup>
+        {options}
+      </FormGroup>
     </Container>
   );
 }

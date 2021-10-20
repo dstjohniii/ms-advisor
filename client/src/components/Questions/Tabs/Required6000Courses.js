36dbff1742252b861facb340962f1a4b8d56a4a3
Gabriel Wallace <35503105+gwallace04@users.noreply.github.com>
2021-10-20 13:29:34 -0500

    Update 6000 level course tab (#41)
    * Add radiogroup to select preferred 6000 level course
    
    * Refactor variable declaration

--- client/src/components/Questions/Tabs/Required6000Courses.js
+++ client/src/components/Questions/Tabs/Required6000Courses.js
@@ -1,7 +1,33 @@
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
+import courses from "../../../data/ClassInfo.json";
 
 export default function Required6000Courses() {
+  
+  const courses6000 = courses.filter(
+    (v) => v.courseNum.toString().charAt(0) === '6'
+    );
+
+  const courseLabels = courses6000.map(
+    (a) => `${a.subject} ${a.courseNum} - ${a.courseName}`
+    );
+
+  const options = courseLabels.map((item) => {
+    return (
+      <FormControlLabel 
+        key={item} 
+        value={item} 
+        control={<Radio />} 
+        label={item}
+      >
+        {item}
+      </FormControlLabel>
+      )
+    });
+
   return (
     <Container
       sx={{
@@ -11,7 +37,12 @@ export default function Required6000Courses() {
       }}
     >
       <Typography variant="h2">6000 Level Required Courses</Typography>
-      <Typography>6000 level required courses go here</Typography>
+      <Typography>Please select your preferred 6000 level course</Typography>
+      <RadioGroup
+        name="6000-lvl-group"
+      >
+        {options}
+      </RadioGroup>
     </Container>
   );
 }

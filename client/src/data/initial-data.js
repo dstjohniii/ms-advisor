ac66bdd182b01bb521f1168438680123058af754
Denis <dstjohn@outlook.com>
2021-10-26 12:12:33 -0500

    feat: highlights semester on task start drag

--- client/src/data/initial-data.js
+++ client/src/data/initial-data.js
@@ -6,44 +6,44 @@ const initialData = {
       title: "Available Classes",
       taskIds: [],
     },
-    "sp-2022": {
-      id: "sp-2022",
+    "SP-2022": {
+      id: "SP-2022",
       title: "Spring 2022",
       taskIds: [],
     },
-    "su-2022": {
-      id: "su-2022",
+    "SS-2022": {
+      id: "SS-2022",
       title: "Summer 2022",
       taskIds: [],
     },
-    "fa-2022": {
-      id: "fa-2022",
+    "FS-2022": {
+      id: "FS-2022",
       title: "Fall 2022",
       taskIds: [],
     },
-    "sp-2023": {
-      id: "sp-2023",
+    "SP-2023": {
+      id: "SP-2023",
       title: "Spring 2023",
       taskIds: [],
     },
-    "su-2023": {
-      id: "su-2023",
+    "SS-2023": {
+      id: "SS-2023",
       title: "Summer 2023",
       taskIds: [],
     },
-    "fa-2023": {
-      id: "fa-2023",
+    "FS-2023": {
+      id: "FS-2023",
       title: "Fall 2023",
       taskIds: [],
     },
   },
   columnOrder: [
-    "sp-2022",
-    "su-2022",
-    "fa-2022",
-    "sp-2023",
-    "su-2023",
-    "fa-2023",
+    "SP-2022",
+    "SS-2022",
+    "FS-2022",
+    "SP-2023",
+    "SS-2023",
+    "FS-2023",
   ],
 };
 

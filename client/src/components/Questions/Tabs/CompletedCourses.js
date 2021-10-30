4d7d8709bcade412412bd4037fbfb82d5b71e788
Gabriel Wallace <gabe.wallace04@gmail.com>
2021-10-29 22:05:59 -0500

    Add or remove course from object if checked or unchecked respectively

--- client/src/components/Questions/Tabs/CompletedCourses.js
+++ client/src/components/Questions/Tabs/CompletedCourses.js
@@ -7,29 +7,33 @@ import Checkbox from "@mui/material/Checkbox";
 import courses from "../../../data/ClassInfo.json";
 
 export default function CompletedCourses() {
-  const [checkedItems, setCheckedItems] = useState({});
+  const [compCourses, setCompCourses] = useState({});
 
   const handleChange = (e) => {
-    setCheckedItems({...checkedItems, [e.target.name] : e.target.checked });
+    if (e.target.checked) {
+      setCompCourses({...compCourses, [e.target.id] : e.target.value});
+    } else {
+      delete compCourses[e.target.id];
+      setCompCourses({...compCourses})
+    }
   }
 
   useEffect(() => {
-    console.log("checkedItems: ", checkedItems);
-  }, [checkedItems]);
+    console.log("checkedItems: ", compCourses);
+  }, [compCourses]);
 
   const options = courses.map((item) => {
     let label = `${item.subject} ${item.courseNum} - ${item.courseName}`
-    let name = `${item.subject}${item.courseNum}`
+    let id = `${item.subject.charAt(0)}${item.courseNum}`
     
     return (
       <FormControlLabel
         label={label}
         key={label}
         value={item}
-        control={<Checkbox name={name} onChange={handleChange}/>}
+        control={<Checkbox id={id} value={item} onChange={handleChange}/>}
       />
     );
-
   });
 
   return (

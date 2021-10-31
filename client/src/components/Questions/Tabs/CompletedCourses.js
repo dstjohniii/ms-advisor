86e29fd7afa8a7b2b2ed36871ea341574a8fd751
Gabriel Wallace <gabe.wallace04@gmail.com>
2021-10-31 12:35:29 -0500

    Keep items checked when rerendering component

--- client/src/components/Questions/Tabs/CompletedCourses.js
+++ client/src/components/Questions/Tabs/CompletedCourses.js
@@ -4,12 +4,18 @@ import FormGroup from "@mui/material/FormGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import { useEffect } from "react";
 import Checkbox from "@mui/material/Checkbox";
+import { get as _get } from "lodash"
 import courses from "../../../data/ClassInfo.json";
 
 export default function CompletedCourses(params) {
   const handleChange = (e) => {
+    //I had to pass the value as a string and then reconvert to object
+    //Passing as an object just returns a string "[object Object]"
+    //If someone smarter than me can make this less messy, go ahead
+    let course = JSON.parse(e.target.value);
+    course['isChecked'] = e.target.checked;
     if (e.target.checked) {
-      params.setCompCourses({...params.compCourses, [e.target.id] : e.target.value});
+      params.setCompCourses({...params.compCourses, [e.target.id] : course});
     } else {
       delete params.compCourses[e.target.id];
       params.setCompCourses({...params.compCourses})
@@ -23,13 +29,25 @@ export default function CompletedCourses(params) {
   const options = courses.map((item) => {
     let label = `${item.subject} ${item.courseNum} - ${item.courseName}`
     let id = `${item.subject.charAt(0)}${item.courseNum}`
+    let strItem = JSON.stringify(item);
+    //If never checked, 'isChecked' is undefined
+    //Use short-circut evaluation to set undefined values to false
+    //Makes MUI happy
+    let checked = _get(params.compCourses, [id, 'isChecked']) || false;
     
     return (
       <FormControlLabel
+        key={id}
+        //To supress warnings about <div> cannot appear as a descendant of <p>.
+        component={'span'}
         label={label}
-        key={label}
-        value={item}
-        control={<Checkbox id={id} value={item} onChange={handleChange}/>}
+        control={
+          <Checkbox 
+            id={id} 
+            checked={checked} 
+            value={strItem} 
+            onChange={handleChange}
+          />}
       />
     );
   });

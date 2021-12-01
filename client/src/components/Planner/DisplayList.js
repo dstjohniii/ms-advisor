117a4ff4b248d3944879f7468215e9e3aeb090c5
Gabriel Wallace <gabe.wallace04@gmail.com>
2021-11-30 18:02:33 -0600

    Check waived courses in displayList

--- client/src/components/Planner/DisplayList.js
+++ client/src/components/Planner/DisplayList.js
@@ -3,7 +3,7 @@ import _union from "lodash/union";
 import _intersection from "lodash/intersection";
 import CheckIcon from '@mui/icons-material/Check';
 import WarningIcon from '@mui/icons-material/Warning';
-import Tooltip, { tooltipClasses }  from "@mui/material/Tooltip";
+import Tooltip, { tooltipClasses } from "@mui/material/Tooltip";
 import { styled } from '@mui/material/styles';
 import React from "react";
 
@@ -20,69 +20,71 @@ const CustomTooltip = styled(({ className, ...props }) => (
 }));
 
 export default function DisplayList(props) {
-	function getCourseCount() {
-		const takenOrPlanned = _union(props.tabInfo.completed, props.plannedCourses);
-		const courses = _intersection(takenOrPlanned, props.displayArray);
-		return courses.length;
-	}
+  function getCourseCount() {
+    const takenOrPlanned = _union(props.tabInfo.completed, props.plannedCourses);
+    const courses = _intersection(takenOrPlanned, props.displayArray);
+    return courses.length;
+  }
 
-	let subheader;
-	if (props.isCertificate) {
-		subheader = 
-			<CustomTooltip 
-				placement="top-start"
-				title={
-					<div>
-						<Typography> Required: </Typography>
-						{props.reqCourses.map((a) => {
-							return(
-								<List dense>
-									<ListItem sx={{py: 0}}> 
-										<ListItemText sx={{py: 0}} primary={`Comp Sci ${a}`} />
-									</ListItem>
-								</List>
-							)
-						})
-						}
-						<Typography> Electives: </Typography>
-						{props.eleCourses.map((a) => {
-							return(
-								<List dense>
-									<ListItem sx={{py: 0}}> 
-										<ListItemText sx={{py: 0}} primary={`Comp Sci ${a}`} />
-									</ListItem>
-								</List>
-							)
-						})
-						}
-					</div>
-				}
-			>
-				<ListSubheader> {props.subheader}: {getCourseCount()} / {props.total} </ListSubheader>
-			</CustomTooltip>
-	} else {
-		subheader = 
-				<ListSubheader> {props.subheader}: {getCourseCount()} / {props.total} </ListSubheader>
-	}
+  let subheader;
+  if (props.isCertificate) {
+    subheader =
+      <CustomTooltip
+        placement="top-start"
+        title={
+          <div>
+            <Typography> Required: </Typography>
+            {props.reqCourses.map((a) => {
+              return (
+                <List dense>
+                  <ListItem sx={{ py: 0 }}>
+                    <ListItemText sx={{ py: 0 }} primary={`Comp Sci ${a}`} />
+                  </ListItem>
+                </List>
+              )
+            })
+            }
+            <Typography> Electives: </Typography>
+            {props.eleCourses.map((a) => {
+              return (
+                <List dense>
+                  <ListItem sx={{ py: 0 }}>
+                    <ListItemText sx={{ py: 0 }} primary={`Comp Sci ${a}`} />
+                  </ListItem>
+                </List>
+              )
+            })
+            }
+          </div>
+        }
+      >
+        <ListSubheader> {props.subheader}: {getCourseCount()} / {props.total} </ListSubheader>
+      </CustomTooltip>
+  } else {
+    subheader =
+      <ListSubheader> {props.subheader}: {getCourseCount()} / {props.total} </ListSubheader>
+  }
 
-	return(
-		<List sx={{py: 0}}>
-			{subheader}
-			{props.displayArray.sort().map((a) => {
-					let isTakenOrPlanned = 
-						props.tabInfo.completed.includes(a) || props.plannedCourses.includes(a);
-					let color = (isTakenOrPlanned) ? "green" : "red";
-					let primary = `Comp Sci ${a}`;
-					return(
-					<ListItem sx={{py: 0}}>
-							<ListItemText sx={{py: 0, px: 2, color: color}} primary={primary}/> 
-							{isTakenOrPlanned 
-								? <CheckIcon style={{fill: "green"}} /> 
-								: <WarningIcon style={{fill: "red"}}/>
-							}
-					</ListItem>
-					);
-			})}
-		</List>
-	);
+  return (
+    <List sx={{ py: 0 }}>
+      {subheader}
+      {props.displayArray.sort().map((a) => {
+        let isTakenOrPlanned =
+          props.tabInfo.completed.includes(a)
+          || props.plannedCourses.includes(a)
+          || props.tabInfo.waived.includes(a);
+        let color = (isTakenOrPlanned) ? "green" : "red";
+        let primary = `Comp Sci ${a}`;
+        return (
+          <ListItem sx={{ py: 0 }}>
+            <ListItemText sx={{ py: 0, px: 2, color: color }} primary={primary} />
+            {isTakenOrPlanned
+              ? <CheckIcon style={{ fill: "green" }} />
+              : <WarningIcon style={{ fill: "red" }} />
+            }
+          </ListItem>
+        );
+      })}
+    </List>
+  );
 }

d368e69d7ea76b6cc0f84e9646f3b5f25f03cb07
cRaeber <54545269+cRaeber@users.noreply.github.com>
2021-12-02 21:40:39 -0600

    Update initial-data.js

--- client/src/data/initial-data.js
+++ client/src/data/initial-data.js
@@ -6,62 +6,8 @@ const initialData = {
       title: "Available Classes",
       taskIds: [],
     },
-    "SP-2022": {
-      id: "SP-2022",
-      title: "Spring 2022",
-      taskIds: [],
-    },
-    "SS-2022": {
-      id: "SS-2022",
-      title: "Summer 2022",
-      taskIds: [],
-    },
-    "FS-2022": {
-      id: "FS-2022",
-      title: "Fall 2022",
-      taskIds: [],
-    },
-    "SP-2023": {
-      id: "SP-2023",
-      title: "Spring 2023",
-      taskIds: [],
-    },
-    "SS-2023": {
-      id: "SS-2023",
-      title: "Summer 2023",
-      taskIds: [],
-    },
-    "FS-2023": {
-      id: "FS-2023",
-      title: "Fall 2023",
-      taskIds: [],
-    },
-    "SP-2024": {
-      id: "SP-2024",
-      title: "Spring 2024",
-      taskIds: [],
-    },
-    "SS-2024": {
-      id: "SS-2024",
-      title: "Summer 2024",
-      taskIds: [],
-    },
-    "FS-2024": {
-      id: "FS-2024",
-      title: "Fall 2024",
-      taskIds: [],
-    },
   },
   columnOrder: [
-    "SP-2022",
-    "SS-2022",
-    "FS-2022",
-    "SP-2023",
-    "SS-2023",
-    "FS-2023",
-    "SP-2024",
-    "SS-2024",
-    "FS-2024",
   ],
 };
 
